8441cf94bab87f389e22495175f219c90bf9aea7
Mathsterk <mathiassterk@gmail.com>
2016-07-27 21:21:20 +0200

    test

--- Fiscord/Fiscord/Program.cs
+++ Fiscord/Fiscord/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace Fiscord
 {
+    using Discord;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().Start();
+
+        private DiscordClient _client;
+
+        public void Start()
         {
+            _client = new DiscordClient();
+
+            _client.MessageReceived += async (s, e) =>
+            {
+                if (!e.Message.IsAuthor)
+                    await e.Channel.SendMessage(e.Message.Text);
+            };
+
+            _client.ExecuteAndWait(async () => {
+                await _client.Connect("aaaaabbbbbbcccccdddddeeeeefffffggggg")
+            });
         }
     }
+}
 }
