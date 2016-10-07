74a739f449da2addd08d57eafef480007a6e62f6
Mathsterk <mathiassterk@gmail.com>
2016-10-07 22:14:41 +0200

    Revert "shit's broken"
    This reverts commit f8810d0f598ff4bcce01b5f183541d5cda244aa0.

--- Fiscord/Fiscord/Program.cs
+++ Fiscord/Fiscord/Program.cs
@@ -7,12 +7,7 @@ using System.Threading.Tasks;
 namespace Fiscord
 {
     using Discord;
-    using Discord.Audio;
     using System.Diagnostics;
-    using NAudio;
-    using NAudio.Wave;
-    using NAudio.CoreAudioApi;
-
 
     class Program
     {
@@ -20,14 +15,8 @@ namespace Fiscord
 
         private DiscordClient _client;
 
-        public Channel VoiceChannel { get; private set; }
-
         public void Start()
         {
-
-
-
-
             _client = new DiscordClient();
 
             _client.MessageReceived += async (s, e) =>
@@ -64,30 +53,8 @@ namespace Fiscord
             _client.ExecuteAndWait(async () =>
             {
                 await _client.Connect("MjA3OTQzNDA5NjQ4OTI2NzIw.Cnqd5A.6M7E6XWfGVpEdeR1tS5OoENuik4");
-
-                _client.UsingAudio(x => // Opens an AudioConfigBuilder so we can configure our AudioService
-                {
-                    x.Mode = AudioMode.Outgoing; // Tells the AudioService that we will only be sending audio
-                });
-
-                var voiceChannel = _client.FindServers("Testservereren").FirstOrDefault().VoiceChannels.FirstOrDefault(); // Finds the first VoiceChannel on the server 'Music Bot Server'
-
-                var _vClient = Discord.Audio.AudioService; // We use GetService to find the AudioService that we installed earlier. In previous versions, this was equivelent to _client.Audio()
-                .Join(VoiceChannel); // Join the Voice Channel, and return the IAudioClient.
-
-
                 Debug.WriteLine("auth");
             });
-
-            _client.UsingAudio(x => // Opens an AudioConfigBuilder so we can configure our AudioService
-            {
-
-                x.Mode = AudioMode.Outgoing; // Tells the AudioService that we will only be sending audio
-            });
-
-
         }
     }
 }
-
-
